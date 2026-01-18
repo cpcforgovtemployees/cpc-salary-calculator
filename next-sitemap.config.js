cc9372dfc004b339d067b5775bade08a061a6630
cpcforgovtemployees <cpcforgovtemployees@gmail.com>
2026-01-18 21:57:16 +0530

    Fixed sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://indianpaycalculator.in",
+  // 1. Fixes the Redirect Error (Critical)
+  siteUrl: "https://www.indianpaycalculator.in",
+  
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "weekly",
   priority: 0.7,
+
+  // 2. Makes it a single, simple file (Highly Recommended)
+  generateIndexSitemap: false, 
 };
